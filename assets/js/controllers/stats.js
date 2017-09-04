bc4acb7e7c946307deb0bd63974c9a01ca991bcb
greendpd <greendpd@msn.com>
2017-09-04 13:15:27 -0500

    adding some view stats

--- assets/js/controllers/stats.js
+++ assets/js/controllers/stats.js
@@ -1,14 +1,24 @@
 app.controller('statsCtrl',function($scope,$http,gameSrvc,httpSrvc){
   gameSrvc.clearAllPlayers();
-  $scope.user="";
   $scope.chartFollowPercent="";
+  $scope.gamesPlayed=""
+  $scope.averageBet="";
 
 
   $http({
     method: "GET",
     url: "/api/getStats/1",
   }).then((response)=>{
-    console.log(response);
+    let gamesFollowedChart=0;
+    let totalBet=0;
+    response.data.forEach((cur,i,arr)=>{
+      if(cur.followchart){
+        gamesFollowedChart++;
+      }
+
+    })
+    $scope.chartFollowPercent=100*(gamesFollowedChart/response.data.length).toFixed(4);
+    $scope.gamesPlayed=response.data.length;
   })
 
 })
